79f23bcccfd133ef5de1339a78d8993b35947578
Cyril LAWSON <cyril.lawson.ext@sodexo.com>
2023-12-18 09:57:02 +0100

    [CatMash]  - Cleanup unused imports  - Update README

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # CatMash
 
 Java/angular application to vote for the cutest cat
+
+To run the app you can use docker or manual run.
+
+For Docker, u need to have Docker installed on ur computer and in the current folder run:
+
+```bash
+docker build - t catmash -p 8081:8081 .
+```
+This will create a container with catmash image
+
+Do the same for the frontend app, the app will be available on:
+
+`localhost:8081/api/v1`
+
+For manual run open the project in ur favorite editor and run as a SpringBoot Application
+
+Feel free to contact me on 
+```email
+cyril.lawson2022@gmail.com
+```
--- src/main/java/com/catmash/config/WebMvcConfig.java
+++ src/main/java/com/catmash/config/WebMvcConfig.java
@@ -1,15 +1,11 @@
 package com.catmash.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableWebMvc
-public class WebMvcConfig implements WebMvcConfigurer
-{
+public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
@@ -22,14 +18,4 @@ public class WebMvcConfig implements WebMvcConfigurer
                 .allowCredentials(false)
                 .maxAge(MAX_AGE_SECS);
     }
-    /*@Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-            }
-        };
-    }*/
 }
